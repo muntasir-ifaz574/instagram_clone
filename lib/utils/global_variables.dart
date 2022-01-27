bf0fffa637c945a9b3993f923f85d6299b045889
muntasir-ifaz574 <muntasir574@gmail.com>
2022-01-28 03:47:15 +0600

    Profile

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/add_post_screen.dart';
 import 'package:instagram_clone/screens/feed_screen.dart';
+import 'package:instagram_clone/screens/profile_screen.dart';
 import 'package:instagram_clone/screens/search_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 const webScreenSize = 600;
 
-const homeScreenItems = [
+List<Widget> homeScreenItems = [
   FeedScreen(),
   SearchScreen(),
   AddPostScreen(),
   Text('notification'),
-  Text('profile')
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ),
 ];
